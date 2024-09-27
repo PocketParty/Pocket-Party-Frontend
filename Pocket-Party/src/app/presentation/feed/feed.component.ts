4b12e29bcb03b09a5202ce43d6f122dbfc1f3813
Mirlla Marques <mirllamsa@gmail.com>
2024-09-27 12:15:35 -0300

    show product modal

--- Pocket-Party/src/app/presentation/feed/feed.component.ts
+++ Pocket-Party/src/app/presentation/feed/feed.component.ts
@@ -17,14 +17,15 @@ export class FeedComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  product: {name: string, descripition: string, image: string, preco: string};
+  product: {name: string, descripition: string, image: string, preco: string, category: boolean[]};
 
   openModal(produto: any) {
     this.product = {
       name: 'Loja de Fulano',
       descripition: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       image: 'https://auhekids.com.br/wp-content/uploads/2024/03/Desvendando-o-Codigo-da-Festa-Infantil-O-Segredo-do-Sucesso-1.jpg',
-      preco: '10'
+      preco: '10',
+      category: [false, false, false, false, false, true]
     }
     this.showModal = true;
   }
