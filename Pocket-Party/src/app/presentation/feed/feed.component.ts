02969e9149d5570f70d93c670acc79d009893f18
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-10-14 15:49:51 -0300

    Em progresso

--- Pocket-Party/src/app/presentation/feed/feed.component.ts
+++ Pocket-Party/src/app/presentation/feed/feed.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StoreService } from 'src/app/shared/services/store.service';
@@ -10,7 +11,7 @@ import { StoreService } from 'src/app/shared/services/store.service';
 
 export class FeedComponent implements OnInit {
 
-  constructor(private router : Router, private storeService: StoreService) { }
+  constructor(private router : Router, private storeService: StoreService,private http: HttpClient) { }
 
   showModal = false;
 
@@ -22,7 +23,32 @@ export class FeedComponent implements OnInit {
   items: any;
 
   ngOnInit(): void {
-    this.items = this.storeService.getStores();
+	this.http.get<any>(`http://localhost:3000/empresas/pesquisar/todas`, {}).subscribe({
+		next: (response) => {
+			console.log('Empresas pesquisada com sucesso:', response);
+			
+			// Mapeia os dados da resposta para o formato desejado
+			const mappedStores = response.map((empresa: any) => ({
+				id: empresa.id.toString(),
+				name: empresa.nome,
+				descripition: empresa.descricao,
+				type: 'STORE',
+				category: [true, false, true, false, true, true, true], // Exemplo, ajuste conforme necessário
+				image: 'https://placekitten.com/300/200' // Adicione uma imagem padrão ou a partir da resposta
+			}));
+			
+			// Envia os dados mapeados para o StoreService
+			this.storeService.setStores(mappedStores);
+		},
+		error: (error) => {
+			console.error('Erro ao pesquisar empresa:', error);
+		},
+		complete: () => {
+			console.log('Request completed');
+		}
+	});
+	this.items = this.storeService.getStores();
+	
   }
 
   closeModal(event: Event) {
